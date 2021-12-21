271409e61b224291795d77e0b04037090c400f36
Idan Ezer <Idan.Ezer1@s.afeka.ac.il>
2021-12-21 12:09:25 +0200

    Form validation.

--- app/src/main/java/com/example/weshare/activities/ActivityRegistration.java
+++ app/src/main/java/com/example/weshare/activities/ActivityRegistration.java
@@ -8,9 +8,20 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.example.weshare.R;
+import com.example.weshare.support.Validator;
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.android.material.textview.MaterialTextView;
 
 public class ActivityRegistration extends AppCompatActivity {
+
+    private TextInputLayout reg_EDT_username;
+    private TextInputLayout reg_EDT_password;
+    private TextInputLayout reg_EDT_name;
+    private TextInputLayout reg_EDT_phone;
+    private TextInputLayout reg_EDT_email;
+
+    private TextInputLayout[] allFields;
     private MaterialButton reg_BTN_register;
 
     @Override
@@ -21,6 +32,38 @@ public class ActivityRegistration extends AppCompatActivity {
         findViews();
         initBTNs();
 
+        checkFormValidation();
+
+    }
+
+    private boolean isValid() {
+        for (TextInputLayout field : allFields) {
+            if (field.getError() != null || field.getEditText().getText().toString().isEmpty())
+                return false;
+        }
+        return true;
+    }
+
+    private void checkFormValidation() {
+        Validator.Builder
+                .make(reg_EDT_username)
+                .addWatcher(new Validator.Watcher_Username("Invalid username"))
+                .build();
+
+        Validator.Builder
+                .make(reg_EDT_password)
+                .addWatcher(new Validator.Watcher_Password("Invalid password"))
+                .build();
+
+        Validator.Builder
+                .make(reg_EDT_phone)
+                .addWatcher(new Validator.Watcher_Phone("Invalid phone number"))
+                .build();
+
+        Validator.Builder
+                .make(reg_EDT_email)
+                .addWatcher(new Validator.Watcher_Email("Invalid email"))
+                .build();
     }
 
     private void initBTNs() {
@@ -31,15 +74,31 @@ public class ActivityRegistration extends AppCompatActivity {
     }
 
     private void register() {
-        // register
+        if (isValid()) {
+            Toast.makeText(this, "Registration completed!", Toast.LENGTH_LONG).show();
+            finish();
+            Intent intent = new Intent(this, ActivityStart.class);
+            startActivity(intent);
+        }
 
-        Toast.makeText(this, "Registration completed!", Toast.LENGTH_LONG).show();
-        finish();
-        Intent intent = new Intent(this, ActivityStart.class);
-        startActivity(intent);
+        else
+            Toast.makeText(this, "One or more field are invalid!", Toast.LENGTH_LONG).show();
     }
 
     private void findViews() {
+        reg_EDT_username = findViewById(R.id.reg_EDT_username);
+        reg_EDT_password = findViewById(R.id.reg_EDT_password);
+        reg_EDT_name = findViewById(R.id.reg_EDT_name);
+        reg_EDT_phone = findViewById(R.id.reg_EDT_phone);
+        reg_EDT_email = findViewById(R.id.reg_EDT_email);
         reg_BTN_register = findViewById(R.id.reg_BTN_register);
+
+        allFields = new TextInputLayout[] {
+                reg_EDT_username,
+                reg_EDT_password,
+                reg_EDT_name,
+                reg_EDT_phone,
+                reg_EDT_email
+        };
     }
 }
--- app/src/main/java/com/example/weshare/objects/User.java
+++ app/src/main/java/com/example/weshare/objects/User.java
@@ -1,12 +1,13 @@
 package com.example.weshare.objects;
 
 public class User {
+    private String username;
     private String name;
-    private String address;
+    private double[] address;
+    private String phone;
     private String email;
     private String password;
     private int donations;
     private int received;
-
 
 }
